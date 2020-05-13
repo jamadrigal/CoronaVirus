41f235675cf12dcbe1e89c9d3bb20f18391756b7
jamadrigal <jse.mdrgl@gmail.com>
2020-05-13 12:59:09 -0700

    set up API route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Cards from "./components/Cards/Cards.jsx";
+import Charts from "./components//Chart/Charts.jsx";
+import CountryPicker from "./components/CountryPicker/CountryPicker.jsx";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+    //console.log(data);
+  }
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1></h1>
+        <Cards data={this.state.data} />
+        <CountryPicker />
+        <Charts />
+      </div>
+    );
+  }
 }
 
 export default App;
